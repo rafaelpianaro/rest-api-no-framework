569b5d01fb7b70f9d49bf9f6675e5aa0cb48861e
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-18 22:01:01 -0300

    added validations for http req, res

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const { allowedNodeEnvironmentFlags } = require('process')
 const products = require('./data/products')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Hello World</h1>')
-    res.end()
+    if(req.url === '/api/products' && req.method === 'GET'){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHead(404,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({message: 'Route Not Found'}))
+    }
 })
 
 const PORT = process.env.PORT || 5000
