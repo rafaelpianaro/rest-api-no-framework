b0723da4979a87d7e7cede08f835930ff3fa9d06
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-18 22:45:33 -0300

    added findById

---
+++ controllers/productController.js
@@ -0,0 +1,35 @@
+const Product = require('../models/productModel')
+
+// @desc Gets All Products
+// @route GET /api/products
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll()
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify(products))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// @desc Gets Single Products
+// @route GET /api/products/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+        if(!product) {
+            res.writeHead(404,{'Content-Type':'application/json'})
+            res.end(JSON.stringify({message:'Product Not Found'}))
+        } else {
+            res.writeHead(200,{'Content-Type':'application/json'})
+            res.end(JSON.stringify(product))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProduct
+}
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require('http')
-const { allowedNodeEnvironmentFlags } = require('process')
-const products = require('./data/products')
+const { getProducts, getProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products' && req.method === 'GET'){
-        res.writeHead(200,{'Content-Type':'application/json'})
-        res.end(JSON.stringify(products))
+       getProducts(req,res)
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3]
+        getProduct(req, res, id)
     } else {
         res.writeHead(404,{'Content-Type':'application/json'})
         res.end(JSON.stringify({message: 'Route Not Found'}))
